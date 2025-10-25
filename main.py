e2d09d23c2fce563ebfb94d9e12b1865adeec031
brandon_b <brandondobby@ntlworld.com>
2025-10-25 06:12:36 +0100

    main.py & stats.py updated to show dictionary of characters & their counts

--- main.py
+++ main.py
@@ -2,11 +2,14 @@ def get_book_text(filepath):
     with open(filepath) as f:
         return f.read()
 
-from stats import count_words
+from stats import count_words, get_character_counts
 
 def main():
     book_text = get_book_text("books/frankenstein.txt")
     num_words = count_words(book_text)
     print(f"Found {num_words} total words")
+
+    character_counts = get_character_counts(book_text)
+    print(character_counts)
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,10 @@
 def count_words(text):
     words = text.split()
     return len(words)
+
+def get_character_counts(text: str) -> dict[str, int]:
+    counts: dict[str, int] = {}
+    for characters in text.lower():
+        counts[characters] = counts.get(characters, 0) + 1
+    return counts
+
