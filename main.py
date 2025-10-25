fa7ffca4b8e2d166a8a67b5ca1ecda09d24c97ca
brandon_b <brandondobby@ntlworld.com>
2025-10-25 07:49:25 +0100

    specific and long winded main.py & stats.py updates

--- main.py
+++ main.py
@@ -1,15 +1,30 @@
+from stats import count_words, get_character_counts, sort_character_counts
+
+
+
 def get_book_text(filepath):
     with open(filepath) as f:
         return f.read()
 
-from stats import count_words, get_character_counts
 
 def main():
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
     book_text = get_book_text("books/frankenstein.txt")
     num_words = count_words(book_text)
     print(f"Found {num_words} total words")
 
     character_counts = get_character_counts(book_text)
-    print(character_counts)
+
+    sorted_chars = sort_character_counts(character_counts)
+
+    print("--------- Character Count -------")
+    for item in sorted_chars:
+        char = item["char"]
+        count = item["num"]
+        if char.isalpha():
+            print(f"{char}: {count}")
+    print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -8,3 +8,11 @@ def get_character_counts(text: str) -> dict[str, int]:
         counts[characters] = counts.get(characters, 0) + 1
     return counts
 
+def sort_character_counts(char_counts_dict: dict) -> list[dict]:
+    char_list = []
+    for char, count in char_counts_dict.items():
+        char_list.append({"char": char, "num": count})
+    def sort_on(dict_item):
+        return dict_item["num"]
+    char_list.sort(key=sort_on, reverse=True)
+    return char_list
